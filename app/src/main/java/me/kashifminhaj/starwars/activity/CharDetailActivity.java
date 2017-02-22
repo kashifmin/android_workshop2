a21d65753052762c85cc63ca5385811aef5dbcad
Kashif Minhaj <dailypctips@gmail.com>
2017-02-22 23:06:00 +0530

    Create an Activity to display detailed character info

---
+++ app/src/main/java/me/kashifminhaj/starwars/activity/CharDetailActivity.java
@@ -0,0 +1,15 @@
+package me.kashifminhaj.starwars.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.kashifminhaj.starwars.R;
+
+public class CharDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_detail);
+    }
+}
