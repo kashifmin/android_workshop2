3b64bf5fce2c738bb95b7e083b497d8eb9b37fef
Kashif Minhaj <dailypctips@gmail.com>
2017-03-11 23:32:05 +0530

    Display character details in CharDetailActivity

--- app/src/main/java/me/kashifminhaj/starwars/activity/CharDetailActivity.java
+++ app/src/main/java/me/kashifminhaj/starwars/activity/CharDetailActivity.java
@@ -3,6 +3,8 @@ package me.kashifminhaj.starwars.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,17 +15,26 @@ public class CharDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_CHAROBJ = "char_obj";
     private JSONObject charObj;
+    TextView name, dob, height;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_char_detail);
 
+        name = (TextView) findViewById(R.id.name);
+        dob = (TextView) findViewById(R.id.dob);
+        height = (TextView) findViewById(R.id.height);
+
         // Retrieve arguments sent through the intent that started this activity
         Intent i = getIntent();
         if(i.hasExtra(EXTRA_CHAROBJ)) {
             try {
                 charObj = new JSONObject(i.getStringExtra(EXTRA_CHAROBJ));
+                name.setText(charObj.getString("name"));
+                dob.setText(charObj.getString("birth_year"));
+                height.setText(charObj.getString("height"));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -31,6 +42,7 @@ public class CharDetailActivity extends AppCompatActivity {
             // something went wrong!
             finish(); // just kill this activity
         }
+
 
 
 
